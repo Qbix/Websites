29ac11f5e53808e4ab1ecf1228607bdc78f23020
Greg Magarshak <greg@qbix.com>
2024-12-04 01:51:02 -0500

    Make an abstract of the article and stash in stream content on save

--- handlers/Websites/before/Streams_Stream_save_Websites_article.php
+++ handlers/Websites/before/Streams_Stream_save_Websites_article.php
@@ -6,9 +6,22 @@ function Websites_before_Streams_Stream_save_Websites_article($params)
 	$modifiedFields = $params['modifiedFields'];
 
 	if (!empty($modifiedFields['article'])) {
+		// sanitize the whole article
 		$tree = Q_Tree::createAndLoad(Q_CONFIG_DIR.DS.'sanitize'.DS.'html.json');
 		$whitelist = $tree->expect('sanitize', 'whitelist');
-		$stream->article = Q_Html::sanitize($stream->article, $whitelist);
+		$article = Q_Html::sanitize($stream->article, $whitelist);
+		$stream->article = $article;
+		// Now summarize just the text, in the article
+		// TODO: have option to AI to make a summary, when available
+		$whitelist = array('#text' => true);
+		$content = Q_Html::sanitize($article, $whitelist);
+		$content = trim(preg_replace('/\s\s+/', ' ', str_replace(
+			array('&nbsp;', "\n"),
+			array(' ', ' '),
+			$content
+		)));
+		$a = new Streams_Stream();
+		$stream->content = substr($content, 0, $a->maxSize_content() - 100);
 	}
 
 	if ($stream->wasRetrieved()) {
